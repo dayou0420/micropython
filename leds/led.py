197f5a2cf78537846eafffe1b3b17353ad2f7856
mo <dayoujianghong@gmail.com>
2023-12-14 19:21:09 +0900

    edit led.py

--- leds/led.py
+++ leds/led.py
@@ -1,32 +1,32 @@
-from machine import Pin, PWM
+from machine import Pin
 from time import sleep
 
-# Define pins for LEDs
-led_pins = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-leds = [Pin(pin, Pin.OUT) for pin in led_pins]
+class LEDController:
+    def __init__(self, pin_numbers):
+        self.led_pins = pin_numbers
+        self.leds = [Pin(pin, Pin.OUT) for pin in self.led_pins]
 
-# Define pin for the buzzer
-buzzer = PWM(Pin(16))
-melody = [262, 294, 330, 349, 392, 440, 494, 523]
+    def light_leds(self):
+        # Turn on each LED one by one and wait for 1 second
+        for led in self.leds:
+            led.value(1)
+            sleep(1)
+            led.value(0)
 
-def play_song():
-    for note in melody:
-        buzzer.freq(note)
-        buzzer.duty_u16(500)
-        sleep(0.5)
-        buzzer.duty_u16(0)
-        sleep(0.1)
-
-def light_leds():
-    for led in leds:
-        led.value(1)
-        sleep(1)
-        led.value(0)
+    def run_sequence(self, num_repeats):
+        # Repeat the LED sequence specified number of times
+        for _ in range(num_repeats):
+            self.light_leds()
 
 def main():
-    for _ in range(5):
-        light_leds()
-        play_song()
+    # Define pins for LEDs
+    led_pins = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+
+    # Create an instance of LEDController
+    led_controller = LEDController(led_pins)
+
+    # Run the LED sequence 5 times
+    led_controller.run_sequence(5)
 
 if __name__ == "__main__":
     main()
